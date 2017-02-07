90ac02327872c71da305c6d20c5d07dff93c24d3
ismetsezer <ismetsezer@localhost.localdomain>
2017-02-07 18:05:28 +0300

    added Coveralls module

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,4 @@
-
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "ym_data_tree"
 require 'coveralls'
-
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 Coveralls.wear!
+require "ym_data_tree"
